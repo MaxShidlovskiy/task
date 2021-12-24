01345865b4d68b7a0c457d83a7090db0117cb1ef
pafnytiy <shidloba@gmail.com>
2021-12-24 13:22:45 +0300

    work with if else

--- Числа/1/task1.js
+++ Числа/1/task1.js
@@ -1,5 +1,5 @@
-const katet1 = prompt('Длина катета А');
-const katet2 = prompt('Длина катета В');
+const katet1 = +(prompt('Длина катета А'));
+const katet2 = +(prompt('Длина катета В'));
 
 const S = ((1/2) * katet1 * katet2).toFixed(1);
 console.log (S);
